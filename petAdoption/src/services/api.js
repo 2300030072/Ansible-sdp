f61c2cc7aef5990e214ee3bb76eec8a023cb61a0
BARMA ASWINI <144126739+2300030072@users.noreply.github.com>
2025-11-28 01:47:37 +0530

    Update API base URL to include '/api' path

--- petAdoption/src/services/api.js
+++ petAdoption/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:30025';
+const API_BASE_URL = 'http://localhost:30025/api';
 
 // Create axios instance with default config
 const api = axios.create({
